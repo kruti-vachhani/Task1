bb89f29a86d7e056caddd5495c92541daa9a2bd4
kruti-vachhani <tatva.pctr25@outlook.com>
2025-05-14 11:01:16 +0530

    first commit

---
+++ UserManagement/Models/Persons.cs
@@ -0,0 +1,19 @@
+namespace UserManagement.Models;
+
+public class Persons
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; }
+
+    public string Lastname { get; set; }
+
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string Role { get; set; }
+
+}
